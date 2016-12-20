56f8e805b712e4190f1a57b718449f637a95ad87
Arthur A. Venturin <arthur.alves.venturin@gmail.com>
2016-12-20 15:20:04 -0200

    First commit

---
+++ src/Calculadora.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Calculadora;
+
+class Calculadora{
+
+}
---
+++ tests/CalculadoraTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Calculadora\Calculadora;
+
+class CalculadoraTest extends TestCase
+{
+
+}
