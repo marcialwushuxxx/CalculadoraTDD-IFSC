936e7c54a73106a3c12a8d59133035b97c033091
Arthur A. Venturin <arthur.alves.venturin@gmail.com>
2016-12-20 15:21:16 -0200

    Adicionado método de soma, testes em vermelho. A seguir implementar método soma na classe concreta

--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -7,5 +7,11 @@ use Calculadora\Calculadora;
 
 class CalculadoraTest extends TestCase
 {
-
+	public function testSimpleSum()
+	{
+		$a = 1;
+		$b = 2;
+		
+		$this->assertEquals(3, $this->calculadora->sum($a,$b));
+	}
 }
